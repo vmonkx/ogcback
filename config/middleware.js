4f34a1e0bc8512aebb117aa7b022a61f3fce273e
Vladyslav <b.vladyslaw@gmail.com>
2021-03-16 21:55:40 +0300

    add middleware settings

---
+++ config/middleware.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      origin: [
+        "http://localhost",
+        "https://ogcsite-aejq45hrx-vmonkx.vercel.app/",
+      ],
+    },
+  },
+};
